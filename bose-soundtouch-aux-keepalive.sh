0181330cad0ec4369b62270a922bbd5223a97158
Bertrand Martel <matsuo@free.fr>
2017-11-24 10:55:17 +0100

    Initial commit

---
+++ bose-soundtouch-aux-keepalive.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+soundtouch_ip=192.168.0.42
+source=$(curl -s http://$soundtouch_ip:8090/now_playing | xmllint --xpath 'string(/nowPlaying/@source)' -)
+
+if [ $source = "AUX" ]
+then
+        echo SoundTouch mode is AUX: keep-alive
+        curl -s -o /dev/null --request POST --header "Content-Type: application/xml" --data '<key state="press" sender="Gabbo">THUMBS_UP</key>' http://$soundtouch_ip:8090/key
+elif [ $source = "STANDBY" ]
+then
+        echo SoundTouch mode is STANDBY: switch to AUX
+        curl -s -o /dev/null --request POST --header "Content-Type: application/xml" --data '<ContentItem source="AUX" sourceAccount="AUX"></ContentItem>' http://$soundtouch_ip:8090/select
+fi
