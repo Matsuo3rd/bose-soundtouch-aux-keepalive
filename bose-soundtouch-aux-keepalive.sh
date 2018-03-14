7749bb034edcfeec10a8e8f4f4c16de7316032a6
Bertrand Martel <matsuo@free.fr>
2018-03-14 12:43:45 +0100

    Update bose-soundtouch-aux-keepalive.sh
    Switch to bash (instead of sh) for NAS Synology compatibility.
    thanks @dhckdgjs

--- bose-soundtouch-aux-keepalive.sh
+++ bose-soundtouch-aux-keepalive.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+PATH=$PATH:/opt/bin:/usr/bin/:/opt/bin/bash
 
 soundtouch_ip=192.168.0.42
 source=$(curl -s http://$soundtouch_ip:8090/now_playing | xmllint --xpath 'string(/nowPlaying/@source)' -)
